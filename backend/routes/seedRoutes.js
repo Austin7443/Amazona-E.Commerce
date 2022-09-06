0692284238ff8248f45aba49c6cf2e3361f13aa6
austin <austin7443@gmail.com>
2022-09-06 21:51:36 +0100

    Publish-To-Heroku

--- backend/routes/seedRoutes.js
+++ backend/routes/seedRoutes.js
@@ -8,7 +8,7 @@ export const seedRouter = express.Router()
 seedRouter.get('/', async (req, res) => {
     await Product.remove({});
     const createdProducts = await Product.insertMany(data.products);
-    await Product.remove({});
-    const createdUsers = await User.insertMany(data.users);
-    res.send({ createdProducts, createdUsers });
+    // await Product.remove({});
+    // const createdUsers = await User.insertMany(data.users);
+    res.send({ createdProducts });
 })
